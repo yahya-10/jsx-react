84db119842794f72ebadcf8c175e66aa966ff661
Yahya Akermi <yahyaakermi1992@gmail.com>
2020-09-30 17:34:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
+// import './myVideo.mp4';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div style={{maxWidth: '100vw' , backgroundColor: 'ThreeDLightShadow'}}>
+          <h1 className="title red">Yahya Akermi</h1>
+          <br />
+          <img src="/imageInSrc.jpg" />
+          <br />
+          <img src="/imageInPublic.jpg" />
+        </div>
+        <video width={320} height={240} controls>
+          <source src="/myVideo.mp4" type="video/mp4" />
+        </video>
+      </div>
     </div>
   );
 }
